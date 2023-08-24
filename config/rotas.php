0e628cf8fb669199b461aef67ae3fb0c29317710
Mikaele Mendonça <mikaelemendonca@gmail.com>
2023-08-23 21:49:43 -0300

    Adicionada cadastro de formação

--- config/rotas.php
+++ config/rotas.php
@@ -5,8 +5,11 @@ return [
     '/fazer-login' => \Alura\Armazenamento\Controller\Login::class,
     '/logout' => \Alura\Armazenamento\Controller\Logout::class,
     '/novo-curso' => \Alura\Armazenamento\Controller\FormularioInsercaoCurso::class,
+    '/nova-formacao' => \Alura\Armazenamento\Controller\FormularioInsercaoFormacao::class,
     '/salvar-curso' => \Alura\Armazenamento\Controller\PersistenciaCurso::class,
+    '/salvar-formacao' => \Alura\Armazenamento\Controller\PersistenciaFormacao::class,
     '/listar-cursos' => \Alura\Armazenamento\Controller\ListaDeCursos::class,
+    '/listar-formacoes' => \Alura\Armazenamento\Controller\ListaDeFormacoes::class,
     '/editar-curso' => \Alura\Armazenamento\Controller\FormularioEdicaoCurso::class,
     '/excluir-curso' => \Alura\Armazenamento\Controller\ExclusaoDeCurso::class,
 ];
